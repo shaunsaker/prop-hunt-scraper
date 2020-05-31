eccb8455bfbd108fd49ef81769fe5bd1ac91fe1e
Shaun Saker <sakershaun@gmail.com>
2020-05-31 14:18:40 +0200

    Update deletePropertiesWithoutTitleDeeds

--- src/deletePropertiesWithoutTitleDeeds/index.ts
+++ src/deletePropertiesWithoutTitleDeeds/index.ts
@@ -6,10 +6,13 @@ export const deletePropertiesWithoutTitleDeeds = async () => {
     .keys()
     .valueOf();
 
+  let count = 0;
   for (const key of keys) {
     const property = db.get(`properties.${key}`).value();
 
     if (!property.titleDeed) {
+      count += 1;
+      console.log(`Removing property: ${count}`);
       db.get('properties')
         .unset(key)
         .write();
