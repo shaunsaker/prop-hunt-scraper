dff14e4affaf0cc8a05e914c81153996301cd3ba
Shaun Saker <sakershaun@gmail.com>
2020-06-06 11:33:34 +0200

    Move db funcs to database module

--- src/database/index.ts
+++ src/database/index.ts
@@ -7,3 +7,32 @@ const db = low(adapter) as low.LowdbSync<Database>;
 db.defaults(initialState).write();
 
 export { db };
+
+export const findExactOrPartialMatchInDb = (
+  node: keyof Database,
+  value: string,
+) => {
+  // Try an exact match first
+  const match = db.get(`${node}.${value}`).value();
+
+  if (match) {
+    return match;
+  }
+
+  // Try partial match
+  const partialMatch = db
+    .get(node)
+    // @ts-ignore filter does exist
+    .filter(
+      item =>
+        value.includes(item.name) ||
+        value
+          .split(' ')
+          .join('')
+          .includes(item.name),
+    )
+    .first()
+    .value();
+
+  return partialMatch;
+};
